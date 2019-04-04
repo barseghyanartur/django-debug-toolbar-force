cd757c20860f1c08c876fa5ae50c9a1caf1489c8
Artur Barseghyan <artur.barseghyan@gmail.com>
2019-04-04 16:05:05 +0200

    upgrade test suite

--- scripts/test_django_1_10.sh
+++
@@ -1,5 +0,0 @@
-reset
-./scripts/uninstall.sh
-./scripts/install_django_1_10.sh
-#python examples/simple/manage.py test debug_toolbar_force --settings=settings_django_1_10 --traceback -v 3
-./runtests.py
