4b94aec427b065a750b49771f3de9c4f0913bb39
Artur Barseghyan <artur.barseghyan@gmail.com>
2019-05-16 13:06:50 +0200

    fix travis

--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd examples/simple/
-./manage.py runserver --traceback -v 3
+./manage.py runserver --traceback -v 3 --settings=settings.dev "$@"
 
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 reset
 ./scripts/uninstall.sh
 ./scripts/install.sh
-python examples/simple/manage.py test debug_toolbar_force --traceback -v 3
+python examples/simple/manage.py test debug_toolbar_force --traceback -v 3 --settings=settings.dev "$@"
